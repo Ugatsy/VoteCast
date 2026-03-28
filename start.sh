24cbeb1b35227f8c2c9081188ec0e51ca400cf91
Ugatsy <jamesongaluteravalera@gmail.com>
2026-03-29 01:52:13 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Clearing cache..."
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+
+echo "Caching config..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
 
@@ -8,4 +19,7 @@ echo "Starting PHP-FPM..."
 php-fpm -D
 
 echo "Starting Nginx..."
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' &
+
+echo "Tailing Laravel log..."
+tail -f /var/www/storage/logs/laravel.log
